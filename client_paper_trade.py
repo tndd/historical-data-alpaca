4c8f5a8ffae473669ae8adf3c40bed4738c88854
tan <ttru702@gmail.com>
2021-06-03 23:07:50 +0900

    update class vars format

--- client_paper_trade.py
+++ client_paper_trade.py
@@ -12,39 +12,39 @@ logger = getLogger(__name__)
 
 @dataclass
 class ClientPaperTrade:
-    api_key: str
-    secret_key: str
-    base_url: str
-    store_path = './data'
-    assets_file_name = 'assets.yaml'
-    symbol_dl_progress_file_name = 'symbol_dl_progress.yaml'
+    _api_key: str
+    _secret_key: str
+    _base_url: str
+    _store_dir_path = './data'
+    _assets_name = 'assets.yaml'
+    _symbol_dl_progress_name = 'symbol_dl_progress.yaml'
 
     def __post_init__(self) -> None:
-        self.assets_path = f"{self.store_path}/{self.assets_file_name}"
-        self.symbol_dl_progress_path = f"{self.store_path}/{self.symbol_dl_progress_file_name}"
-        self.symbol_dl_progress = self.load_symbol_dl_progress()
+        self._assets_path = f"{self._store_dir_path}/{self._assets_name}"
+        self._symbol_dl_progress_path = f"{self._store_dir_path}/{self._symbol_dl_progress_name}"
+        self._symbol_dl_progress = self.load_symbol_dl_progress()
 
     def get_auth_headers(self) -> dict:
         return {
-            "APCA-API-KEY-ID": self.api_key,
-            "APCA-API-SECRET-KEY": self.secret_key
+            "APCA-API-KEY-ID": self._api_key,
+            "APCA-API-SECRET-KEY": self._secret_key
         }
 
     def get_assets(self) -> dict:
-        url = f"{self.base_url}/assets"
+        url = f"{self._base_url}/assets"
         r = requests.get(url, headers=self.get_auth_headers())
         return r.json()
 
     def download_assets(self) -> None:
         assets = self.get_assets()
-        with open(self.assets_path, 'w') as f:
+        with open(self._assets_path, 'w') as f:
             yaml.dump(assets, f, indent=2)
 
     def load_assets(self) -> dict:
         # if not exist assets data, download it.
-        if not os.path.exists(self.assets_path):
+        if not os.path.exists(self._assets_path):
             self.download_assets()
-        with open(self.assets_path, 'r') as f:
+        with open(self._assets_path, 'r') as f:
             assets = yaml.safe_load(f)
         return assets
 
@@ -64,32 +64,32 @@ class ClientPaperTrade:
                 'm': ''
             }
             symbol_dl_progress[s] = detail
-        with open(self.symbol_dl_progress_path, 'w') as f:
+        with open(self._symbol_dl_progress_path, 'w') as f:
             yaml.dump(symbol_dl_progress, f, indent=2)
 
     def load_symbol_dl_progress(self) -> dict:
-        if not os.path.exists(self.symbol_dl_progress_path):
+        if not os.path.exists(self._symbol_dl_progress_path):
             self.create_symbol_dl_progress()
-        with open(self.symbol_dl_progress_path, 'r') as f:
+        with open(self._symbol_dl_progress_path, 'r') as f:
             symbol_dl_progress = yaml.safe_load(f)
         return symbol_dl_progress
 
     def symbols_progress_todo(self) -> list:
-        return [s for s, d in self.symbol_dl_progress.items() if d['f'] is False]
+        return [s for s, d in self._symbol_dl_progress.items() if d['f'] is False]
 
     def update_symbol_dl_progress(self) -> None:
-        with open(self.symbol_dl_progress_path, 'w') as f:
-            yaml.dump(self.symbol_dl_progress, f, indent=2)
+        with open(self._symbol_dl_progress_path, 'w') as f:
+            yaml.dump(self._symbol_dl_progress, f, indent=2)
 
     def update_dl_progress_of_symbol(
             self,
             symbol: str,
             is_complete: bool,
             message: str = '') -> None:
-        if symbol in self.symbol_dl_progress:
-            self.symbol_dl_progress[symbol]['t'] = datetime.datetime.now()
-            self.symbol_dl_progress[symbol]['f'] = is_complete
-            self.symbol_dl_progress[symbol]['m'] = message
+        if symbol in self._symbol_dl_progress:
+            self._symbol_dl_progress[symbol]['t'] = datetime.datetime.now()
+            self._symbol_dl_progress[symbol]['f'] = is_complete
+            self._symbol_dl_progress[symbol]['m'] = message
             self.update_symbol_dl_progress()
         else:
             logger.error(f"symbol \"{symbol}\" is not exist.")
@@ -98,9 +98,9 @@ class ClientPaperTrade:
 def main():
     load_dotenv()
     client = ClientPaperTrade(
-        api_key=os.getenv('ALPACA_API_KEY'),
-        secret_key=os.getenv('ALPACA_SECRET_KEY'),
-        base_url=os.getenv('ALPACA_ENDPOINT_PAPER_TRADE')
+        _api_key=os.getenv('ALPACA_API_KEY'),
+        _secret_key=os.getenv('ALPACA_SECRET_KEY'),
+        _base_url=os.getenv('ALPACA_ENDPOINT_PAPER_TRADE')
     )
     client.update_dl_progress_of_symbol('UNCH', False, '')
 
