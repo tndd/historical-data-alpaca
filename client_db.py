f810259ea29efcd1c1bbf4ad456ef31959dfbfc8
tan <ttru702@gmail.com>
2021-06-06 00:48:18 +0900

    implement feature create table

--- client_db.py
+++ client_db.py
@@ -21,6 +21,7 @@ class ClientDB:
         self._conn = self.create_connection()
         self._cur = self._conn.cursor()
         self.init_db()
+        self.create_table_historical_bars_1min()
 
     def create_connection(self) -> mysql.connector.MySQLConnection:
         return mysql.connector.connect(
@@ -32,6 +33,21 @@ class ClientDB:
     def init_db(self) -> None:
         self._cur.execute(f"CREATE DATABASE IF NOT EXISTS {self._name};")
         self._cur.execute(f"USE {self._name};")
+
+    def create_table_historical_bars_1min(self) -> None:
+        query = '''
+                CREATE TABLE IF NOT EXISTS `historical_bars_1min` (
+                    `time` datetime NOT NULL,
+                    `symbol` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+                    `open` double NOT NULL,
+                    `high` double NOT NULL,
+                    `low` double NOT NULL,
+                    `close` double NOT NULL,
+                    `volume` mediumint unsigned NOT NULL,
+                    PRIMARY KEY (`time`,`symbol`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                '''
+        self._cur.execute(query)
 
 
 def main():
