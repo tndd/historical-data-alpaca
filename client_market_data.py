e1b5a0af9ec37d614b9785f4a0f919f2588fc80e
tan <ttru702@gmail.com>
2021-06-06 23:10:57 +0900

    read timeframe from class var

--- client_market_data.py
+++ client_market_data.py
@@ -81,8 +81,8 @@ class ClientMarketData(ClientAlpaca):
             is_complete=True
         )
 
-    def load_bars_lines(self, symbol: str, timeframe: str = '1Min'):
-        bars_dir_path = f"{self._dl_bars_destination}/{symbol}/{timeframe}"
+    def load_bars_lines(self, symbol: str):
+        bars_dir_path = f"{self._dl_bars_destination}/{symbol}/{self._time_frame}"
         bars_paths = glob.glob(f"{bars_dir_path}/*.yaml")
         # download bars data if not exist it.
         if len(bars_paths) == 0:
