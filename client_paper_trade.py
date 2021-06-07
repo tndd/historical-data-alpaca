71d41292ade73dcb7400bb1c1c00487aaeb79092
tan <ttru702@gmail.com>
2021-06-07 18:43:17 +0900

    fix log text

--- client_paper_trade.py
+++ client_paper_trade.py
@@ -22,19 +22,19 @@ class ClientPaperTrade(ClientAlpaca):
     def get_assets(self) -> dict:
         url = f"{self._base_url}/assets"
         r = requests.get(url, headers=self.get_auth_headers())
-        self._logger.debug(f"request status code: \"{r.status_code}\"")
+        self._logger.debug(f"Request status code: \"{r.status_code}\"")
         return r.json()
 
     def download_assets(self) -> None:
         assets = self.get_assets()
         with open(self._assets_path, 'w') as f:
             yaml.dump(assets, f, indent=2)
-        self._logger.debug(f"assets is downloaded in \"{self._assets_path}\"")
+        self._logger.debug(f"Assets is downloaded in \"{self._assets_path}\"")
 
     def load_assets(self) -> dict:
         # if not exist assets data, download it.
         if not os.path.exists(self._assets_path):
-            self._logger.debug('assets data is not exist, it will be downloaded.')
+            self._logger.debug('Assets data is not exist, it will be downloaded.')
             self.download_assets()
         self._logger.debug(f'Loading assets data.')
         time_start = datetime.datetime.now()
@@ -69,7 +69,7 @@ class ClientPaperTrade(ClientAlpaca):
 
     def load_symbol_dl_progress(self) -> dict:
         if not os.path.exists(self._symbol_dl_progress_path):
-            self._logger.debug('symbol_dl_progress is not exist, it will be created.')
+            self._logger.debug('"symbol_dl_progress" is not exist, it will be created.')
             self.init_symbol_dl_progress()
         with open(self._symbol_dl_progress_path, 'r') as f:
             symbol_dl_progress = yaml.safe_load(f)
@@ -89,7 +89,7 @@ class ClientPaperTrade(ClientAlpaca):
     def update_symbol_dl_progress(self) -> None:
         with open(self._symbol_dl_progress_path, 'w') as f:
             yaml.dump(self._symbol_dl_progress, f, indent=2)
-        self._logger.debug(f"in class's \"symbol_data_progress\" is saved in {self._symbol_dl_progress_path}")
+        self._logger.debug(f"In class's \"symbol_data_progress\" is saved in {self._symbol_dl_progress_path}")
 
     def update_dl_progress_of_symbol(
             self,
@@ -129,7 +129,7 @@ class ClientPaperTrade(ClientAlpaca):
 
     def is_symbol_exist(self, symbol: str) -> bool:
         if not (symbol in self._symbol_dl_progress.keys()):
-            self._logger.debug(f"symbol \"{symbol}\" is not exist in symbol_dl_progress.")
+            self._logger.debug(f"Symbol \"{symbol}\" is not exist in symbol_dl_progress.")
             return False
         return True
 
@@ -144,7 +144,7 @@ class ClientPaperTrade(ClientAlpaca):
             self._symbol_dl_progress[symbol][market_dt.value][price_dt.value][time_frame.value]['dl_until_time']
         )
         if dl_comp_date != '':
-            self._logger.debug(f"symbol \"{symbol}\" is already downloaded.")
+            self._logger.debug(f"Symbol \"{symbol}\" is already downloaded.")
             return True
         return False
 
