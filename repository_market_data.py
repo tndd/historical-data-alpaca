c07a80c1967c4a63ffe1f86c87a70b30c95ad300
tan <ttru702@gmail.com>
2021-06-10 17:14:01 +0900

    separate sql to sql files

--- repository_market_data.py
+++ repository_market_data.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from logging import getLogger, config, Logger
 from client_market_data import ClientMarketData
 from client_db import ClientDB
+from data_types import QueryType
 
 os.makedirs('log', exist_ok=True)
 config.fileConfig('logging.conf')
@@ -17,26 +18,18 @@ class RepositoryMarketData:
     _logger: Logger = getLogger(__name__)
     _client_md: ClientMarketData = ClientMarketData()
     _client_db: ClientDB = ClientDB()
+    _tbl_name_bars_min: str = 'bars_1min'
 
     def __post_init__(self) -> None:
-        self._create_table_bars_1min()
+        self._create_tables()
 
-    def _create_table_bars_1min(self) -> None:
-        query = '''
-            CREATE TABLE IF NOT EXISTS `bars_1min` (
-                `time` datetime NOT NULL,
-                `symbol` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-                `open` double NOT NULL,
-                `high` double NOT NULL,
-                `low` double NOT NULL,
-                `close` double NOT NULL,
-                `volume` int unsigned NOT NULL,
-                PRIMARY KEY (`time`,`symbol`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-        '''
-        self._client_db.cur.execute(query)
+    def _create_tables(self) -> None:
+        q_bars_min = self._client_db.load_query_by_name(QueryType.CREATE, self._tbl_name_bars_min)
+        self._client_db.cur.execute(q_bars_min)
+        self._client_db.conn.commit()
 
     def _count_symbol_table_bars_1min(self, symbol: str) -> int:
+        # TODO: delete maybe
         query = f'''
             SELECT COUNT(*)
             FROM bars_1min
@@ -45,16 +38,12 @@ class RepositoryMarketData:
         self._client_db.cur.execute(query)
         return self._client_db.cur.fetchone()[0]
 
-    def _load_table_bars_1min_dataframe(self, symbol: str) -> pd.DataFrame:
-        query = f'''
-            SELECT `time`, symbol, `open`, high, low, `close`, volume
-            FROM alpaca_market_db.bars_1min
-            WHERE symbol = '{symbol}'
-            order by time
-        '''
-        return pd.read_sql(query, self._client_db.conn)
+    def _load_bars_min_dataframe(self, symbol: str) -> pd.DataFrame:
+        query = self._client_db.load_query_by_name(QueryType.SELECT, self._tbl_name_bars_min)
+        return pd.read_sql(query, self._client_db.conn, params=(symbol,))
 
     def _load_bars_lines_from_files(self, symbol: str) -> list:
+        # TODO: delete maybe. because tmp file for saving is not necessary.
         bars_dir_path = self._client_md.get_dl_bars_destination(symbol)
         bars_paths = glob.glob(f"{bars_dir_path}/*.yaml")
         # if download bars is not completed, it will be downloaded automatically.
@@ -96,20 +85,22 @@ class RepositoryMarketData:
             INSERT INTO alpaca_market_db.bars_1min (`time`, symbol, `open`, high, low, `close`, volume)
             VALUES(%s, %s, %s, %s, %s, %s, %s);
         '''
+        # TODO: bars lines will get from market_data_client directly.
         bars_lines = self._load_bars_lines_from_files(symbol)
         self._client_db.insert_lines(query, bars_lines)
         self._logger.info(f'bars "{symbol}" is stored to db.')
 
     def load_bars_df(self, symbol: str) -> pd.DataFrame:
+        # TODO: delete maybe. because this func can be integrated into "_load_bars_min_dataframe".
         if self._count_symbol_table_bars_1min(symbol) == 0:
             self._logger.info(f'bars "{symbol}" is not exist in db, it will be stored.')
             self._store_bars_to_db(symbol)
-        return self._load_table_bars_1min_dataframe(symbol)
+        return self._load_bars_min_dataframe(symbol)
 
 
 def main():
     rp = RepositoryMarketData()
-    df = rp.load_bars_df('SPY')
+    df = rp._load_bars_min_dataframe('SPY')
     print(df)
 
 
