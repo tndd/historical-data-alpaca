b5da91efc022f529e3cca6984b495ab3883b47bf
tan <ttru702@gmail.com>
2021-06-13 14:03:58 +0900

    add get logger method

---
+++ logger_alpaca/logger_alpaca.py
@@ -0,0 +1,23 @@
+import json
+import os
+from datetime import datetime
+from pathlib import Path
+from logging import config, getLogger, Logger
+
+
+def get_logger(logger_name: str) -> Logger:
+    datetime_now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+    log_file_name = f'{datetime_now}.log'
+    parent_path = Path(__file__).parent
+    log_conf_name = 'logging.json'
+    log_dir_path = f'{parent_path}/log'
+    os.makedirs(log_dir_path, exist_ok=True)
+    with open(f'{parent_path}/{log_conf_name}', 'r') as f:
+        cnf = json.load(f)
+    cnf['handlers']['logFileHandler']['filename'] = f'{log_dir_path}/{log_file_name}'
+    config.dictConfig(cnf)
+    return getLogger(logger_name)
+
+
+if __name__ == '__main__':
+    print(get_logger('test'))
